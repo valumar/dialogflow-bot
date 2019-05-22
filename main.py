9e9226e109e431f1c82dfd98b73b7354913fb3ea
Petr E. Antonov <antpeter@gmail.com>
2019-05-22 22:03:12 +0300

    Initial Telegram Bot

---
+++ main.py
@@ -0,0 +1,57 @@
+import logging
+import os
+
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+from dotenv import load_dotenv
+
+
+load_dotenv()
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO)
+
+logger = logging.getLogger(__name__)
+
+
+def start(bot, update):
+    """Send a message when the command /start is issued."""
+    bot.send_message(chat_id=update.message.chat_id, text='Hi!')
+
+
+def echo(bot, update):
+    """Echo the user message."""
+    bot.send_message(chat_id=update.message.chat_id, text=update.message.text)
+
+
+def error(bot, update):
+    """Log Errors caused by Updates."""
+    logger.warning('Update "%s" caused error "%s"', bot, update.error)
+
+
+def main():
+    """Start the bot."""
+    # Create the Updater and pass it your bot's token.
+    updater = Updater(os.getenv("TELEGRAM_TOKEN"))
+
+    # Get the dispatcher to register handlers
+    dp = updater.dispatcher
+
+    # on different commands - answer in Telegram
+    dp.add_handler(CommandHandler("start", start))
+
+    # on noncommand i.e message - echo the message on Telegram
+    dp.add_handler(MessageHandler(Filters.text, echo))
+
+    # log all errors
+    dp.add_error_handler(error)
+
+    # Start the Bot
+    updater.start_polling()
+
+    # Run the bot until you press Ctrl-C or the process receives SIGINT,
+    # SIGTERM or SIGABRT. This should be used most of the time, since
+    # start_polling() is non-blocking and will stop the bot gracefully.
+    updater.idle()
+
+
+if __name__ == '__main__':
+    main()
